c3b6765b6164ebe982ebbbd1630e4b3a2d86cc1f
Ryan Ore <ryanore@jellyvision.com>
2016-11-02 23:03:09 -0500

    Update array method to return boolean

--- src/utils/array.js
+++ src/utils/array.js
@@ -2,5 +2,5 @@
  * Arrays intersect
  */
 export const intersects = (a1, a2) => {
-  return a1.filter(n => ( a2.indexOf(n) != -1)).length
+  return a1.filter(n => ( a2.indexOf(n) != -1)).length > 0
 }
